97e3dd368709ec585ad658960c99273452672664
MaxAlkan <max.b.garcia0@gmail.com>
2026-04-25 17:02:29 -0700

    Added functionality to allow player ship to rotate left/right

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from constants import LINE_WIDTH
 from constants import PLAYER_RADIUS
+from constants import PLAYER_TURN_SPEED
 import circleshape
 import pygame
 
@@ -17,3 +18,14 @@ class Player(circleshape.CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_RADIUS * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+        if keys[pygame.K_d]:
+            self.rotate(-dt)    
