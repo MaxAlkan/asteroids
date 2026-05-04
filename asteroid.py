64befb302a5aacc07299040a91de5e75a3b2048c
MaxAlkan <max.b.garcia0@gmail.com>
2026-05-04 00:37:43 -0700

    Added logic to asteroid.py to delete asteroids that were no longer on screen. The idea being this helps with performance.

--- asteroid.py
+++ asteroid.py
@@ -10,6 +10,7 @@ from logger import log_event
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius )
+        self.on_screen = False
 
     
     def draw(self, screen):
@@ -17,6 +18,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+        
+        #Added this with the help of Boots to remove Asteroid objects once they were no longer on screen.
+        #The idea is to help with performance so we are not considering Asteroids that are no longer 
+        #in the game space.
+        if not self.on_screen:
+            # check if it has entered the screen yet
+            if (0 <= self.position.x <= SCREEN_WIDTH and 
+                0 <= self.position.y <= SCREEN_HEIGHT):
+                self.on_screen = True
+        else:
+            # only kill it once it has left after entering
+            if (self.position.x < 0
+                or self.position.x > SCREEN_WIDTH
+                or self.position.y < 0
+                or self.position.y > SCREEN_HEIGHT):
+                self.kill()
 
     def split(self):
         self.kill()
